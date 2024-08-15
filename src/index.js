d2826c2776ec287d120dffb8c54856b9b673c905
klintat <98770096+klintat@users.noreply.github.com>
2024-08-15 12:48:46 +0300

    Created modal for new notes

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
-import '../src/style.css'
+import '../src/style.css';
+
+// New note module functionality
+const openNotesModal = document.getElementById("new-note-btn");
+const modal = document.getElementById("note-modal-overlay");
+const closeNotesModal = document.querySelector(".close-modal-btn");
+
+function openModal() {
+    modal.classList.remove("note-modal-hide");
+}
+
+function closeModal(e, clickedOutside) {
+    if (clickedOutside) {
+        if (e.target.classList.contains("note-modal-overlay"))
+            modal.classList.add("note-modal-hide");
+    } else modal.classList.add("note-modal-hide");
+}
+
+openNotesModal.addEventListener("click", openModal);
+modal.addEventListener("click", (e) => closeModal(e, true));
+closeNotesModal.addEventListener("click", closeModal);
